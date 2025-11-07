ee35554320df33f1d68abe935e3a4bfd71e37a53
johnpranoy7 <47387210+johnpranoy7@users.noreply.github.com>
2025-11-07 02:36:30 -0500

    Obstacle jump height + double damage fix  + player physics
    Added Cooldown timer 0.5sec for double damage deduction

--- Assets/2D Platformer/Scripts/PlayerController.cs
+++ Assets/2D Platformer/Scripts/PlayerController.cs
@@ -10,9 +10,12 @@ namespace Platformer
     {
         public float movingSpeed;
         public float jumpForce;
+        public float healthRegenerationRate;
         private float moveInput;
         private bool jumpFlag = false;
         private bool keyboardControlFlag = false;
+        private float lastDamageTime = -Mathf.Infinity; // track last damage time
+        public float damageCooldown = 0.5f; // 0.5 second cooldown
 
         private bool facingRight = false;
         [HideInInspector]
@@ -152,11 +155,12 @@ namespace Platformer
 
         private void OnCollisionEnter2D(Collision2D other)
         {
-            if (other.gameObject.tag == "Enemy")
+            if (other.gameObject.tag == "Enemy" && (Time.time - lastDamageTime > damageCooldown))
             {
                 //deathState = true; // Say to GameManager that player is dead
-                healthBar.setHealth(healthBar.healthSlider.value - 20);
-                rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse); 
+                healthBar.setHealth(healthBar.healthSlider.value - healthRegenerationRate);
+                lastDamageTime = Time.time;  // reset cooldown timer
+                rigidbody.AddForce(transform.up * jumpForce/2, ForceMode2D.Impulse); 
                 Debug.Log("Player hit by enemy. Health: " + healthBar.healthSlider.value);
             }
             
